7d4d06e64d1a24bb620ae4745ea8e7760379d3e3
ashraful1910 <ashraful1910@gmail.com>
2024-08-19 17:56:40 +0600

    customer id

--- resources/views/report056.blade.php
+++ resources/views/report056.blade.php
@@ -20,7 +20,7 @@
                   </ol>
                 </nav>
               </div>
-              
+
             </div>
             <!--end breadcrumb-->
 
@@ -39,11 +39,12 @@
 
             <div class="card-body">
               <div class="table-responsive">
-                
+
 				<table id="example2" class="table table-bordered mb-0">
 					<thead>
 						<tr>
 							<th scope="col" style="border: 1px solid black;text-align: center;">#</th>
+                            <th scope="col" style="border: 1px solid black;text-align: center;">Customer ID</th>
 							<th scope="col" style="border: 1px solid black;text-align: center;">Bill Date</th>
 							<th scope="col" style="border: 1px solid black;text-align: center;">Customer Info</th>
 							<th scope="col" style="border: 1px solid black;text-align: center;">Bill No</th>
@@ -55,36 +56,37 @@
 							<th scope="col" style="border: 1px solid black;text-align: center;">Receive</th>
 						</tr>
 					</thead>
-					<tbody>				
+					<tbody>
 <?php
 
 $result = DB::select("
-select bill_no,job_no,customer_nm,net_bill,received,bonus,due,vat_wav,bill_dt,charge,customer_reg,customer_vehicle
+select bill_no,job_no,customer_id,customer_nm,net_bill,received,bonus,due,vat_wav,bill_dt,charge,customer_reg,customer_vehicle
 from
 (
 SELECT `bill` bill_no, a.`job_no`, b.customer_nm, b.bill_dt,customer_reg,customer_vehicle,
-sum(a.`net_bill`) net_bill, sum(`received`) received, sum(`bonus`) bonus, sum(`due`) due, sum(`vat_wav`) vat_wav, sum(charge) charge
+sum(a.`net_bill`) net_bill, sum(`received`) received, sum(`bonus`) bonus, sum(`due`) due, sum(`vat_wav`) vat_wav, sum(charge) charge, b.customer_id
 FROM `pay` a, bill_mas b, customer_info c
-WHERE 
+WHERE
 a.bill = b.bill_no and b.flag<>'0' and c.customer_id=b.customer_id and a.customer_id=c.customer_id
-and b.work='intercompany' 
-GROUP by bill,job_no,customer_nm,b.bill_dt,customer_reg,customer_vehicle
+and b.work='intercompany'
+GROUP by bill,job_no,customer_id,customer_nm,b.bill_dt,customer_reg,customer_vehicle
 )A
 where due > 1
 order by customer_nm,bill_dt
 ;
 ");
-	$sl = '1'; 	$total02 = '0';	
+	$sl = '1'; 	$total02 = '0';
 foreach($result as $item)
 		{		$total01 = '0';$net_bill = $item->net_bill; $vat= (int)$net_bill*.1;
 		$job_no = $item->job_no;
-$ref='N/A';			
+$ref='N/A';
 $data01 = DB::select("SELECT ref FROM `pay` WHERE `job_no` = '$job_no' AND ref<>'SYS' order by id");
 foreach($data01 as $item01){ $ref=$item01->ref ;}
-		
-		
+
+
 ?>					<tr>
 						<th scope="row" style="border: 1px solid black;text-align: center;">{{$sl}}</th>
+                        <td style="border: 1px solid black;text-align: center;">{{$item->customer_id}}</td>
 						<td style="border: 1px solid black;text-align: center;">{{$item->bill_dt}}</td>
 						<td style="border: 1px solid black;text-align: left;"><b>C/N:</b> {{$item->customer_nm}}
 						<br><b>Car Reg:</b> {{$item->customer_reg}}
@@ -100,52 +102,52 @@ foreach($data01 as $item01){ $ref=$item01->ref ;}
 						</td>
 						<td style="border: 1px solid black;text-align: center;">{{$item->due}}</td>
 						<td style="border: 1px solid black;text-align: center;">{{$ref}}</td>
-					<td style="border: 1px solid black;text-align: center;">	
-<?php 
+					<td style="border: 1px solid black;text-align: center;">
+<?php
 if ((session('role')=="Super Administrator")||(session('role')=="Accounts"))
 {
-?>					
+?>
 					<form style="display: inline;" action="pay" method="post" target="_blank">{{ csrf_field() }}
 					<input type="hidden" name="bill_no" value="{{$item->bill_no}}">
 					<button class="btn btn-outline-success px-3" type="submit" name="" value="">
 					 Receive</button>
 					</form>
-<?php } else {?>						
+<?php } else {?>
 					<form style="display: inline;" action="pay" method="post" target="_blank">
 					<input type="hidden" name="bill_no" value="{{$item->bill_no}}">
 					<button class="btn btn-secondary px-3" type="submit" name="" value="" disabled>
 					 Receive</button>
 					</form>
-<?php } ?>						
-					
+<?php } ?>
+
 					</td>
-					
+
 					</tr>
 		<?php
 		$sl = $sl+1;
 		$total01 = $item->due;
 		$total02 = $total01+$total02;
-		}  
+		}
 ?>
 						<!---tr>
 							<td colspan="7"><strong>Total Draft Amount: Tk.</strong></td>
 							<td>{{$total02}}</td>
 						</tr--->
-						
+
 					</tbody>
 				</table>
-				
+
 <strong>Intercompany Receivable Amount: Tk. {{number_format(($total02), 2, '.', ',')}}	</strong>
-<br><br><br>				
-				
-			
-				
-				
-				
-				
-				
-				
-				
+<br><br><br>
+
+
+
+
+
+
+
+
+
              </div>
 
              <!--end row-->
@@ -153,25 +155,25 @@ if ((session('role')=="Super Administrator")||(session('role')=="Accounts"))
              <hr>
            <!-- begin invoice-note -->
            <div class="my-3">
-            
+
            </div>
          <!-- end invoice-note -->
             </div>
-			
-			
 
-          
+
+
+
            </div>
 
 
-			
-			
+
+
 </main>
 
 
 
-		  
-@endsection		 
+
+@endsection
 
 
 
